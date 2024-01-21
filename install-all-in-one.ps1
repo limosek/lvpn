9e18840ce1b891c17050688b73e865bf5b957e20
Lukas Macura <lukas.macura@gmail.com>
2024-01-21 12:10:31 +0100

    Added all-in-one install script

--- install-all-in-one.ps1
+++ install-all-in-one.ps1
@@ -6,13 +6,17 @@ $lethean_version = "v5.0.1"
 $lvpn_branch = "main"
 $root = "${env:HOMEDRIVE}${env:HOMEPATH}\lvpn"
 
+#############################################################################
 # Create LVPN home directory
+#############################################################################
 if (-not (Test-Path "${root}"))
 {
   mkdir "${root}"
 }
 
+#############################################################################
 # Create LVPN app directory
+#############################################################################
 if (-not (Test-Path "${root}\app"))
 {
   mkdir "${root}\app"
@@ -20,26 +24,41 @@ if (-not (Test-Path "${root}\app"))
 $appdir = "${root}\app"
 $logfile = "${root}\setup.log"
 
-Set-Location $app
+Set-Location $appdir
 
+#############################################################################
+# Download python Application
+#############################################################################
 if (-not (Test-Path "lvpn.zip"))
 {
     Invoke-WebRequest "https://github.com/limosek/lvpn/archive/refs/heads/${lvpn_branch}.zip" -OutFile "lvpn.zip"
 }
-Expand-Archive python.zip -DestinationPath .
+Expand-Archive lvpn.zip -DestinationPath .
+Move-Item lvpn-${lvpn_branch}/* .
+Remove-Item lvpn-${lvpn_branch} -Recurse -Force
 
+#############################################################################
+# Download Lethean CLI tools
+#############################################################################
 if (-not (Test-Path "lethean-cli-windows.zip"))
 {
     Invoke-WebRequest "https://github.com/letheanVPN/blockchain-iz/releases/download/${lethean_version}/lethean-cli-windows.zip" -OutFile lethean-cli-windows.zip
 }
-Expand-Archive lethean-cli-windows.zip
+Expand-Archive lethean-cli-windows.zip .
 
+#############################################################################
+# Download embedded Python
+#############################################################################
 if (-not (Test-Path "python.zip"))
 {
     Invoke-WebRequest "https://www.python.org/ftp/python/${python_version}/python-${python_version}-embed-amd64.zip" -OutFile "python.zip"
 }
 Expand-Archive python.zip -DestinationPath python
 Set-Location python
+
+#############################################################################
+# Download PIP
+#############################################################################
 Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
 .\python get-pip.py
 @"
@@ -48,11 +67,26 @@ Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
 .\Lib\site-packages
 import site
 "@ | Out-File -Encoding utf8 -FilePath python312._pth -Append
-.\python -m pip install virtualenv
 
 $Env:PATH += ";" + (Resolve-Path .\python)
 $Env:PATH += ";" + (Resolve-Path .\python\scripts)
 $Env:PATH += ";" + (Resolve-Path .\bin)
 $Env:PATH += ";" + (Resolve-Path .\lethean-cli-windows)
 
+#############################################################################
+# Install dependencies
+#############################################################################
+Set-Location ..
 .\python\python -m pip install -r requirements.txt
+
+#############################################################################
+# Create shortcut
+#############################################################################
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut([Environment]::GetFolderPath("Desktop") + "\lvpn.lnk")
+$Shortcut.TargetPath = "${appdir}/lvpnc.cmd"
+$Shortcut.IconLocation = "${appdir}/config/icon.ico"
+$Shortcut.WorkingDirectory = "${appdir}"
+$Shortcut.Save()
+
+Write-Output OK
--- lvpnc.cmd
+++ lvpnc.cmd
@@ -1,5 +1,4 @@
 @echo off
 
-PATH="%~dp0\virtualenv\scripts;%~dp0;%~dp0\bin;%~dp0\python;%PATH%"
-call "%~dp0\virtualenv\scripts\activate.bat"
-start "%~dp0\virtualenv\scripts\pythonw" "%~dp0\virtualenv\scripts\pythonw" "%~dp0\client.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
+PATH="%~dp0\python\scripts;%~dp0;%~dp0\lethean-cli-windows;%~dp0\python;%PATH%"
+.\python\python.exe "%~dp0\client.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
